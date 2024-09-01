c75b5b7aef14109cd4a0c4af25920ff6b78a91b6
WaDelch <W.Delchmann85@gmx.net>
2024-09-01 16:47:30 +0200

    init

---
+++ src/main/java/de/WaDelch/RequestCatcher/RequestCatcherApplication.java
@@ -0,0 +1,13 @@
+package de.WaDelch.RequestCatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestCatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestCatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/WaDelch/RequestCatcher/RequestCatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package de.WaDelch.RequestCatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestCatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
